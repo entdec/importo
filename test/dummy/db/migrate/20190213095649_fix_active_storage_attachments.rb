266ba243081cd88270bd24104b56890c277aef15
Tom de Grunt <tom@degrunt.nl>
2019-02-13 20:23:49 +0100

    Forgot this for the tests

---
+++ test/dummy/db/migrate/20190213095649_fix_active_storage_attachments.rb
@@ -0,0 +1,28 @@
+class FixActiveStorageAttachments < ActiveRecord::Migration[5.2]
+  def change
+    drop_table :active_storage_attachments, if_exists: true
+    drop_table :active_storage_blobs, if_exists: true
+
+    create_table :active_storage_blobs, id: :uuid do |t|
+      t.string   :key,        null: false
+      t.string   :filename,   null: false
+      t.string   :content_type
+      t.text     :metadata
+      t.bigint   :byte_size,  null: false
+      t.string   :checksum,   null: false
+      t.datetime :created_at, null: false
+
+      t.index [ :key ], unique: true
+    end
+
+    create_table :active_storage_attachments, id: :uuid do |t|
+      t.string     :name,     null: false
+      t.references :record,   null: false, polymorphic: true, index: false, type: :uuid
+      t.references :blob,     null: false, type: :uuid
+
+      t.datetime :created_at, null: false
+
+      t.index [ :record_type, :record_id, :name, :blob_id ], name: "index_active_storage_attachments_uniqueness", unique: true
+    end
+  end
+end
