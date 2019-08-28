471eace9ed6f2a83603c941dd49864dae71855f3
Andre Meij <andre@itsmeij.com>
2019-08-28 23:21:07 +0200

    namespacing?

--- lib/importo/engine.rb
+++ lib/importo/engine.rb
@@ -12,7 +12,7 @@ module Importo
       end
     end
 
-    initializer :append_migrations do |app|
+    initializer 'importo.append_migrations' do |app|
       unless app.root.to_s.match?(root.to_s)
         config.paths['db/migrate'].expanded.each do |expanded_path|
           app.config.paths['db/migrate'] << expanded_path
