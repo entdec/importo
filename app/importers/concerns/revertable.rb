ee970ee55d6fac408000cd3a7339f48b4af3b6c8
Andre Meij <andre@itsmeij.com>
2019-09-11 08:35:56 +0200

    Allow to override undo completely

--- app/importers/concerns/revertable.rb
+++ app/importers/concerns/revertable.rb
@@ -7,6 +7,18 @@ module Revertable
   extend ActiveSupport::Concern
 
   def revert!
+    undo_all
+
+    import.reverted!
+  rescue StandardError => e
+    import.result_message = "Exception: #{e.message}"
+    Rails.logger.error "Importo exception: #{e.message} backtrace #{e.backtrace.join(';')}"
+    import.failure!
+  end
+
+  private
+
+  def undo_all
     revertable_results = import.results.select { |result| result['state'] == 'success' }
 
     revertable_results.each do |revertable_result|
@@ -21,19 +33,9 @@ module Revertable
         result['message'] = "Not reverted: #{e.message}"
       end
     end
-
-    @import.result.attach(io: results_file, filename: 'results.xlsx', content_type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
-    @import.result_message += "\nReverted #{results.select { |result| result['state'] == 'reverted' }.size} of #{revertable_results.size} rows"
-    @import.reverted!
-  rescue StandardError => e
-    @import.result_message = "Exception: #{e.message}"
-    Rails.logger.error "Importo exception: #{e.message} backtrace #{e.backtrace.join(';')}"
-    @import.failure!
   end
 
-  private
-
-  def undo(klass, id, _row)
+  def undo_row(klass, id, _row)
     klass.constantize.find(id).destroy
   end
 end
