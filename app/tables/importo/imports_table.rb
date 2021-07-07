92216d0484021bedbaf5c39f70162b77512a067d
Tom de Grunt <tom@degrunt.nl>
2021-07-07 08:47:44 +0200

    Fix link

--- app/tables/importo/imports_table.rb
+++ app/tables/importo/imports_table.rb
@@ -12,12 +12,12 @@ class Importo::ImportsTable < ActionTable::ActionTable
   column(:extra_links, sortable: false) { |import| Importo.config.admin_extra_links(import).map { |name, link| link_to(link[:text], link[:url], title: link[:title], target: '_blank', class: link[:icon]) }}
 
   column :actions, title: '', sortable: false do |import|
-    content_tag(:span, class: 'btn-group btn-group-xs') do
+    content_tag(:span) do
       if import.can_revert?
         concat link_to(content_tag(:i, nil, class: 'fa fa-undo'), importo.undo_import_path(import), method: :post, data: { confirm: 'Are you sure? This will undo this import.' })
       end
       if Importo.config.admin_can_destroy(import)
-        concat link_to(content_tag(:i, nil, class: 'fa fa-trash'), main_app.importo_path(import), method: :delete, class: 'float-right', data: { confirm: 'Are you sure? This will prevent duplicate imports from being detected.' })
+        concat link_to(content_tag(:i, nil, class: 'fa fa-trash'), importo.import_path(import), method: :delete, class: 'float-right', data: { confirm: 'Are you sure? This will prevent duplicate imports from being detected.' })
       end
     end
   end
