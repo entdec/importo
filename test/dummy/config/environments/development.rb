d42ec7e8f185c8fabea8e15eea589e4b200fd58f
Tom de Grunt <tom@degrunt.nl>
2024-05-24 16:43:30 +0200

    Remove legacy stuff

--- test/dummy/config/environments/development.rb
+++ test/dummy/config/environments/development.rb
@@ -54,5 +54,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.active_storage.service = :local
-  config.active_record.legacy_connection_handling = false
 end
