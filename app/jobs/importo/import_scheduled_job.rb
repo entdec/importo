b36807b7d0d32b2cb825b6af31caa6c35a9e8dee
shubh <S.Seetharamasetty@rapidlogistics.nl>
2024-09-12 13:24:13 +0400

    add ImportScheduledJob

--- app/jobs/importo/import_scheduled_job.rb
+++ app/jobs/importo/import_scheduled_job.rb
@@ -1,7 +1,7 @@
 module Importo
   class ImportScheduledJob < ApplicationJob
     def perform()
-      imports = Import.where(state: "scheduled", created_at: ..160.minutes.ago)
+      imports = Import.where(state: "scheduled", created_at: ..30.minutes.ago)
 
       imports.each do |import|
         ImportService.perform_async(import: import)
