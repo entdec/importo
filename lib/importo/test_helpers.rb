4a10384422f75afcddaefa442a8845c03b2e803d
Tom de Grunt <tom@degrunt.nl>
2024-10-02 09:06:43 +0200

    Fix tests

--- lib/importo/test_helpers.rb
+++ lib/importo/test_helpers.rb
@@ -1,5 +1,3 @@
-
-
 # frozen_string_literal: true-
 
 require "axlsx"
@@ -32,7 +30,7 @@ module Importo
       import.save!
 
       ImportService.perform(import: import)
-      ImportJobCallback.new.on_complete({import_id: import.id})
+      ImportJobCallback.new.on_complete("success", {import_id: import.id})
       import
     end
   end
